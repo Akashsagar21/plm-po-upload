7ff12e8831ced38f9575db6c7d518bfbe19b8adc
Akashsagar21 <akashsagar.ind@classichome.com>
2025-10-08 15:31:08 +0530

    added iniatial files

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'plm_po_upload',
+    'version': '1.0',
+    'depends': ['sale_management','product','inspection','contacts'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/sale_order_line_views.xml',
+        'views/res_partner_views.xml',
+        'views/sale_order_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
